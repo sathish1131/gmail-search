2dcb72dc61d4b3c99fd922eed7d364ab1419862c
sathish <sathishkumar1131@gmail.com>
2024-12-01 19:16:01 +0530

    spring boot first commit

---
+++ src/test/java/com/gmail/gmail_search/GmailSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.gmail_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
